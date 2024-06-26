c1f4011dcfedab212047bfa2ec57202336711ee2
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-26 12:06:22 -0600

    got my spells

--- app/controllers/SandboxSpellsController.js
+++ app/controllers/SandboxSpellsController.js
@@ -1,8 +1,15 @@
+import { AppState } from "../AppState.js";
 import { sandboxSpellsService } from "../services/SandboxSpellsService.js";
 import { Pop } from "../utils/Pop.js";
 
 export class SandboxSpellsController {
-  constructor() { }
+  constructor() {
+    // REVIEW cannot get spells on page load because we are not logged in yet!
+    // this.getMySpells()
+
+    // NOTE wait until the user is logged in before getting their spells!
+    AppState.on('account', this.getMySpells)
+  }
 
   async saveSpell() {
     try {
@@ -10,6 +17,15 @@ export class SandboxSpellsController {
     } catch (error) {
       Pop.error(error)
       console.error('COULD NOT SAVE SPELL', error);
+    }
+  }
+
+  async getMySpells() {
+    try {
+      await sandboxSpellsService.getMySpells()
+    } catch (error) {
+      Pop.error(error)
+      console.error('COULD NOT GET MY SPELLS', error);
     }
   }
 }
--- app/services/SandboxSpellsService.js
+++ app/services/SandboxSpellsService.js
@@ -1,10 +1,18 @@
+import { AppState } from "../AppState.js";
 import { api } from "./AxiosService.js"
 
 class SandboxSpellsService {
+  async getMySpells() {
+    const response = await api.get('api/spells')
+
+    console.log('🐕🔮🔮🔮🔮🔮 GETTING MY SPELLS', response.data);
+  }
   async saveSpell() {
-    const response = await api.post('api/spells')
+    const spellToSave = AppState.activeSpell
+    const response = await api.post('api/spells', spellToSave)
 
     console.log('🐕✨🔮 CREATED SPELL', response.data);
+    // TODO maybe do some more stuff here later....
   }
 }
 
